96b82f6ba08b91211a00258c5f826e38491ace4a
vjyhh <ryabovoldenya@gmail.com>
2024-01-02 16:39:17 +0200

    +/game

--- index.js
+++ index.js
@@ -4,13 +4,70 @@ const token = "6773668852:AAHWkNIc-doC8mWo6llIuOuQ3PopbLaWTtU";
 
 const bot = new TelegramAPI(token, { polling: true });
 
-bot.on("message", (msg) => {
-  const text = msg.text;
-  const ChatId = msg.chat.id;
-  // if (ChatId == 5287944367) {
-  //   bot.sendMessage(ChatId, "ты забанен вхвхвхвх");
-  //   return 0;
-  // }
-  bot.sendMessage(ChatId, "ты написал мне " + text);
-  console.log(msg);
+const chats = {}
+
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{ text: '0', callback_data: '0' }, { text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }],
+
+            [{ text: '3', callback_data: '3' }, { text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }],
+
+            [{ text: '6', callback_data: '6' }, { text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }],
+
+            [{ text: '9', callback_data: '9' }]
+        ]
+    })
+}
+
+bot.setMyCommands([
+    { command: '/start', description: 'начало переписки' },
+    { command: 'game', description: 'Игра началась!' },
+])
+
+
+bot.on("message", async(msg) => {
+    const text = msg.text;
+    const ChatId = msg.chat.id;
+    const time_send = new Date(msg.date * 1000);
+
+
+
+    if (text == '/start') {
+
+        if (msg.from.first_name != undefined && msg.from.last_name != undefined) {
+            return bot.sendMessage(ChatId, "Привет, " +
+                msg.from.first_name + " " + msg.from.last_name + "! Это мой телеграм бот.");
+        } else if (msg.from.first_name == undefined) {
+
+            return bot.sendMessage(ChatId, "Привет, " + msg.from.last_name + "! это мой телеграм ${123} бот.");
+        } else if (msg.from.last_name == undefined) {
+            bot.sendMessage(ChatId, "Привет, " + msg.from.first_name + "! Это мой телеграм бот.");
+            // bot.sendMessage(ChatId, "дата отправки вашего сообщения:" + time_send);
+            // console.log(`Дата и время: ${time_send}`);
+        }
+
+    }
+    if (text == "/game") {
+        await bot.sendMessage(ChatId, "Угадай число от 0 до 9!");
+        const rN = Math.floor(Math.random() * 10);
+        bot.sendMessage(ChatId, rN);
+        chats[ChatId] = rN;
+        return bot.sendMessage(ChatId, "Отгадывай!", gameOptions);
+    }
+    // bot.sendMessage(ChatId, "ты написал мне " + text);
+    // console.log(msg);
 });
+
+bot.on("callback_query", async msg => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+    if (data == chats[chatId]) {
+        return await bot.sendMessage(chatId, `Ты выбрал правильное число ${data}`);
+    } else {
+        return await bot.sendMessage(chatId, `Ты выбрал не правильное число ${data}`);
+    }
+    bot.sendMessage(chatId, `Ты выбрал число ${data}`);
+    console.log(msg);
+})
